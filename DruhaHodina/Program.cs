b33377f8b02f75d5fc6a156f661506ec61da07ed
Honza <duy.nguyen@infis.cz>
2025-10-05 20:52:25 +0200

    Doplnění

---
+++ DruhaHodina/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace MyNamespace;
+
+class program
+{
+    private static bool EndsWithSuffix(string input, string suffix)
+    {
+        // input.EndsWith(suffix);
+
+        if (suffix.Length > input.Length)
+        {
+            return false;
+        }
+
+        for (int i = 0; i < suffix.Length; i++)
+        {
+            if (input[input.Length - 1 - i] == suffix[suffix.Length - 1 - i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    
+    private static string ToTitleCase(string input)
+    {
+        var slova = input.Split(" ");
+
+        string celek = "";
+
+        foreach (var slovo in slova)
+        {
+            char capilatised = (char) (slovo[0] - 32);
+            celek += capilatised + slovo.Substring(1);
+        }
+        return celek.TrimEnd();
+    }
+    
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(EndsWithSuffix("Ahoj.txt", "aaaaaaaaaahoj.txt"));
+    }
+}
