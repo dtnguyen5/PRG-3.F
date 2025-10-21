bf257325fff357022cde196dc5ee78ffc64132ed
Honza <duy.nguyen@infis.cz>
2025-10-21 22:28:33 +0200

    OOP IN C#

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿
+string a = Console.ReadLine();
+
+for (int i = 1 ; i < a.Length  ; i++)
+{
+    Console.WriteLine(i);
+}
---
+++ OOP_1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MyNamespace;
+
+class program
+{
+    public static void Main(string[] args)
+    {
+        
+        Book book = new Book(300, true, "Karel Čapek", 1920, "Válka s Mloky");
+        
+    }
+}
