0ee1bb4dcd7232d82594e87d4bbded042dc34866
duy.nguyen <duy.nguyen@infis.cz>
2025-11-04 09:34:19 +0100

    commit

--- OpakovaniNaTest/Coder.cs
+++ OpakovaniNaTest/Coder.cs
@@ -7,6 +7,7 @@ public class Coder
     public Coder(int secret)
     {
         _secret = secret;
+        // _secret = secret % ('z' - 'a') + 1;
     }
 
     public void Encode(string input)
@@ -17,14 +18,43 @@ public class Coder
         // ´a´ posunuty o secret = ´d´
         foreach (var ch in input)
         {
-            int hodnota = 'a' + _secret;
-            char output = (char)hodnota;
-            Console.WriteLine(output);   
+            // int hodnota = 'a' + _secret;
+            // char output = (char)hodnota;
+            // Console.WriteLine(output);
+
+            char znak = (char)(ch + _secret);
+            if (znak > 'z')
+            {
+                int pretek = znak - 'z';
+                Console.Write((char)('a' + pretek));
+            }
+            else
+            {
+                Console.WriteLine(znak);
+            }
+            
         }
+        Console.WriteLine();
     }
 
     public void Decode(string input)
     {
+        input = input.ToLower();
         
+        foreach (var ch in input)
+        {
+            char znak = (char)(ch - _secret);
+            if (znak < 'a')
+            {
+                int pretek = 'a' - znak;
+                Console.Write((char)('z' - pretek));
+            }
+            else
+            {
+                Console.WriteLine(znak);
+            }
+            
+        }
+        Console.WriteLine();
     }
 }
--- OpakovaniNaTest/Program.cs
+++ OpakovaniNaTest/Program.cs
@@ -2,14 +2,63 @@
 
 class Program
 {
+    // 3.Pole (Lodě
+    public static void DrawBoard(int[] arr, bool isVisible)
+    {
+        char[] symbols = { 'w', 'P', 'L', 'D'};
+        foreach (var prvek in arr)
+        {
+            if (isVisible)
+            {
+                Console.WriteLine(symbols[prvek]);   
+            }
+            else
+            {
+                Console.WriteLine(symbols[0]);
+            }
+        }
+
+        Console.WriteLine();
+    }
+    
     public static void Main(string[] args)
     {
+        // 1. Jednoduchá třída
         var Kytara = new Guitar(24, "SCHECTER PT-8 MS Black Ops", "Eben");
         Kytara.PrintInfo();
         Kytara.PrintInfo();
         Kytara.PrintInfo();
-
-        var cd = new Coder(3);
+        
+        // 2. Komplikovanější třída
+        var cd = new Coder(1);
         cd.Encode("ahoj");
+        cd.Decode("Pizza");
+
+        // 3.Pole (Lodě
+        var board = new int[20];
+        var rng = new Random();
+        board[rng.Next(0, board.Length)] = 3;
+        board[rng.Next(0, board.Length)] = 2;
+        board[rng.Next(0, board.Length)] = 1;
+        
+        // board[2] = 3;
+        // board[5] = 1;
+        // board[13] = 2;
+
+        while (true)
+        {
+            DrawBoard(board, false);
+            Console.WriteLine("Vyber si poličko, které chceš zničit");
+            int pos = int.Parse(Console.ReadLine()!);
+            if (board[pos - 1] != 0)
+            {
+                Console.WriteLine("Good job!");
+            }
+            else
+            {
+                Console.WriteLine("Better luck next time :-C");
+            }
+        }
+        
     }
 }
