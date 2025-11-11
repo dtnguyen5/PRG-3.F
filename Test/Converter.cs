290658522fcbebc6713bb4ee2a5ecdd5c2748d38
duy.nguyen <duy.nguyen@infis.cz>
2025-11-11 09:37:14 +0100

    Test A

---
+++ Test/Converter.cs
@@ -0,0 +1,20 @@
+namespace MyNamespace;
+
+public class Converter
+{
+    public int BinToDec(string Number)
+    {
+        return Convert.ToInt32(Number, 2);
+    }
+
+    public string DecToBin(int num)
+    {
+        
+    }
+    
+    public string HexToBin(int Number)
+    {
+       
+    }
+    
+}
